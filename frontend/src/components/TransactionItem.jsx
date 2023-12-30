f1638141c7af46abb8fdc0bde54f40c8894b2a36
Kirit0me <prabhavagarwal444@gmail.com>
2023-12-31 03:22:41 +0530

    folder name change

--- frontend/src/components/TransactionItem.jsx
+++ frontend/src/components/TransactionItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function TransactionItem({ transaction }) {
   const transactionType = transaction.amount >= 0 ? 'Income' : 'Expense';
   return (
-    <li className={`p-2 mb-2 ${transaction.amount >= 0 ? 'text-green-600' : 'text-red-600'}`}>
+    <li class={`p-2 mb-2 ${transaction.amount >= 0 ? 'text-green-600' : 'text-red-600'}`}>
       <b>{transaction.description}</b> - ${transaction.amount.toFixed(2)} ({transactionType})
     </li>
   );
